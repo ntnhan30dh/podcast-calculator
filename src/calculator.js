32c50e44c719604a3572103c6930b9bd0c37bde5
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-10-23 18:16:52 +0200

    function version

--- src/calculator.js
+++ src/calculator.js
@@ -3,53 +3,68 @@ import { Dropdown, Input } from "semantic-ui-react";
 
 import CountryList from "./countryList";
 import Reach from "./aReach";
+import Payment from "./paymentModel";
 
 const Calculator = () => {
   const countryList = new CountryList();
   const aReach = new Reach();
+  const payment = new Payment();
 
-  const payment = [{ key: "CPM", value: "CPM", text: "CPM" },
-  { key: "CPA", value: "CPA", text: "CPA" },]
-  const format = [{ key: "Host-read", value: "Host-read", text: "Host-read" },
-  { key: "pre-recorded", value: "pre-recorded", text: "pre-recorded" },]
-  let cost = 0;
+  const payments = [
+    { key: "cpm", value: "cpm", text: "CPM" },
+    { key: "cpa", value: "cpa", text: "CPA" },
+  ];
+  const format = [
+    { key: "Host-read", value: "Host-read", text: "Host-read" },
+    { key: "pre-recorded", value: "pre-recorded", text: "pre-recorded" },
+  ];
 
   let [region, setRegion] = useState(undefined);
   let [country, setCountry] = useState(undefined);
+  let [reach, setReach] = useState(undefined);
   let [paymentModel, setPaymentModel] = useState(undefined);
   let [adFormat, setAdFormat] = useState(undefined);
-  let [reach, setReach] = useState(undefined);
   let [spot, setSpot] = useState(undefined);
 
-//   const action =() =>{
-//  aReach.transfer()
-//  console.log(
-//    "aReach.transfer()[country]",
-//    aReach.transfer()[country]
-// ,
-//    aReach.transfer()[country].monthly
-//  )
-//   }
+  let paymentAtr =
+    paymentModel === "cpa"
+      ? payment.getPayment(paymentModel)[country]
+      : payment.getPayment(paymentModel)[country] * reach * 0.001;
+  let cost = spot * paymentAtr * adFormat;
 
-  const placeholder = "hehe";
+  const handelSetSpot = (value) => {
+    if (!country) {
+      alert("country is missing");
+    }
+     else if (!paymentModel) {
+      alert("paymentModel is missing");
+    } 
+    else if (paymentModel==='cpm'&&!reach) {
+      alert("reach is missing");
+    } 
+    else if (!adFormat) {
+      alert("adFormat is missing");
+    } 
+    else setSpot(value);
+  };
   return (
     <div>
       <h1>Calculator</h1>
       <h3> Region</h3>
       <Dropdown
         placeholder="Select Region"
-        fluid
+        // fluid
         search
         selection
         options={countryList.regions()}
-      //  onChange={action}
+        // onChange={action}
         onChange={(e, { value }) => setRegion(value)}
       />
       <h3>Country</h3>
       <h4>{country}</h4>
       <Dropdown
         placeholder="Select Country"
-        fluid
+        // fluid
         search
         selection
         options={countryList.countries(region)}
@@ -57,34 +72,52 @@ const Calculator = () => {
       />
       <h3>Reach</h3>
       <h4>{reach}</h4>
-      <Input placeholder={placeholder}  onChange={(e, { value }) => setReach(value)}/>
-      
-      <h3>Average weekly listners: {country&&aReach.aReachArr()[country]&& aReach.aReachArr()[country].weekly} </h3>
-      <h3>Average monthly listners:  {country&&aReach.aReachArr()[country]&& aReach.aReachArr()[country].monthly}</h3>
+      <Input onChange={(e, { value }) => setReach(value)} />
+
+      <h3>
+        Average monthly listners:{" "}
+        {country &&
+          aReach.aReachArr()[country] &&
+          aReach.aReachArr()[country].monthly}
+      </h3>
+      <h3>
+        Average weekly listners:{" "}
+        {country &&
+          aReach.aReachArr()[country] &&
+          aReach.aReachArr()[country].weekly}{" "}
+      </h3>
 
       <h3>Payment model</h3>
       <h4>{paymentModel}</h4>
+      <h3>Payment number</h3>
+      <h4>
+        {country && paymentModel && payment.getPayment(paymentModel)[country]}
+      </h4>
       <Dropdown
         placeholder="Payment model"
-        fluid
+        // fluid
         selection
-        options={payment}
+        options={payments}
         onChange={(e, { value }) => setPaymentModel(value)}
       />
       <h3>Ad format</h3>
       <h4>{adFormat}</h4>
       <Dropdown
-        placeholder="Payment model"
-        fluid
+        placeholder="Ad format"
+        // fluid
         selection
         options={format}
-        onChange={(e, { value }) => setAdFormat(value)}
+        onChange={(e, { value }) =>
+          value === "Host-read" ? setAdFormat(1) : setAdFormat(0.8)
+        }
       />
       <h3>Number of spots</h3>
       <h4>{spot}</h4>
-      <Input  onChange={(e, { value }) => setSpot(value)}/>
+      <Input onChange={(e, { value }) => handelSetSpot(value)} />
 
-      <h2>Estimated cost: {cost}</h2>
+      <h2>
+        Estimated cost: {country && paymentModel && spot && adFormat && cost}
+      </h2>
     </div>
   );
 };
--- src/countryList.js
+++ src/countryList.js
@@ -4,7 +4,6 @@ class CountryList {
     this.countries = this.countries.bind(this);
   }
   regions = () => {
-    
     let res = [
       { key: "Latam", value: "Latam", text: "Latam" },
       { key: "Nordics", value: "Nordics", text: "Nordics" },
@@ -15,115 +14,129 @@ class CountryList {
     return res;
   };
   countries = (region) => {
-    let names =[];
-      switch(region) {
-          case "Latam":
-              names = ["Argentina",
-                "Bolivia",
-                "Chile",
-                "Dominican Republic",
-                "Panama",
-                "Paraguay",
-                "Uruguay",
-                "Venezuela"]
-              break
-              case "Mena":
-                names = ["Egypt",
-                    "Jordan",
-                    "Kuwait",
-                    "Oman",
-                    "Qatar",
-                    "Saudi Arabia",
-                    "Turkey",
-                    "UAE"]
-                break
-                case "Asia":
-                names = ["Bangladesh",
-                    "Cambodia",
-                    "Hong Kong",
-                    "Japan",
-                    "Korea",
-                    "Laos",
-                    "Malaysia",
-                    "Myanmar",
-                    "Pakistan",
-                    "Philippines",
-                    "Singapore",
-                    "Taiwan",
-                    "Thailand"]
-                break
-                case "Nordics":
-                    names = ["Austria",
-                        "Bulgaria",
-                        "Czech Republic",
-                        "Finland",
-                        "Greece",
-                        "Hungary",
-                        "Norway",
-                        "Romania",
-                        "Sweden"]
-                    break
-                    case "CEE":
-                        names = ["Bosnia and Herzegovina",
-                            "Croatia",
-                            "Cyprus",
-                            "Montenegro",
-                            "Serbia"]
-                        break
-      default :
-      names = [
-        "Austria",
-        "Bahrain",
-        "Bangladesh",
-        "Bolivia",
-        "Bosnia and Herzegovina",
-        "Bulgaria",
-        "Cambodia",
-        "Chile",
-        "Colombia",
-        "Croatia",
-        "Cyprus",
-        "Czech Republic",
-        "Dominican Republic",
-        "Egypt",
-        "Finland",
-        "Greece",
-        "Hong Kong",
-        "Hungary",
-        "Japan",
-        "Jordan",
-        "Kuwait",
-        "Laos",
-        "Malaysia",
-        "Montenegro",
-        "Myanmar",
-        "Norway",
-        "Oman",
-        "Pakistan",
-        "Panama",
-        "Paraguay",
-        "Philippines",
-        "Qatar",
-        "Romania",
-        "Saudi Arabia",
-        "Serbia",
-        "Singapore",
-        "South Korea",
-        "Sweden",
-        "Taiwan",
-        "Thailand",
-        "Turkey",
-        "UAE",
-        "Uruguay",
-      ]
-      }
+    let names = [];
+    switch (region) {
+      case "Latam":
+        names = [
+          "Argentina",
+          "Bolivia",
+          "Chile",
+          "Dominican Republic",
+          "Panama",
+          "Paraguay",
+          "Uruguay",
+          "Venezuela",
+        ];
+        break;
+      case "Mena":
+        names = [
+          "Bahrain",
+          "Egypt",
+          "Jordan",
+          "Kuwait",
+          "Oman",
+          "Qatar",
+          "Saudi Arabia",
+          "Turkey",
+          "UAE",
+        ];
+        break;
+      case "Asia":
+        names = [
+          "Bangladesh",
+          "Cambodia",
+          "Hong Kong",
+          "Japan",
+          "Korea",
+          "Laos",
+          "Malaysia",
+          "Myanmar",
+          "Pakistan",
+          "Philippines",
+          "Singapore",
+          "Taiwan",
+          "Thailand",
+        ];
+        break;
+      case "Nordics":
+        names = [
+          "Austria",
+          "Bulgaria",
+          "Czech Republic",
+          "Finland",
+          "Greece",
+          "Hungary",
+          "Norway",
+          "Romania",
+          "Sweden",
+        ];
+        break;
+      case "CEE":
+        names = [
+          "Bosnia and Herzegovina",
+          "Croatia",
+          "Cyprus",
+          "Montenegro",
+          "Serbia",
+        ];
+        break;
+      default:
+        names =[]
+        // names = [
+        //   "Austria",
+        //   "Bahrain",
+        //   "Bangladesh",
+        //   "Bolivia",
+        //   "Bosnia and Herzegovina",
+        //   "Bulgaria",
+        //   "Cambodia",
+        //   "Chile",
+        //   "Colombia",
+        //   "Croatia",
+        //   "Cyprus",
+        //   "Czech Republic",
+        //   "Dominican Republic",
+        //   "Egypt",
+        //   "Finland",
+        //   "Greece",
+        //   "Hong Kong",
+        //   "Hungary",
+        //   "Japan",
+        //   "Jordan",
+        //   "Kuwait",
+        //   "Laos",
+        //   "Malaysia",
+        //   "Montenegro",
+        //   "Myanmar",
+        //   "Norway",
+        //   "Oman",
+        //   "Pakistan",
+        //   "Panama",
+        //   "Paraguay",
+        //   "Philippines",
+        //   "Qatar",
+        //   "Romania",
+        //   "Saudi Arabia",
+        //   "Serbia",
+        //   "Singapore",
+        //   "Korea",
+        //   "Sweden",
+        //   "Taiwan",
+        //   "Thailand",
+        //   "Turkey",
+        //   "UAE",
+        //   "Uruguay",
+        //   "Venezuela",
+        // ];
+    }
     let res = [];
-    names.map(name=>( res.push({
-            key: name, value: name, text: name
-        })
-    
-
-    ))
+    names.map((name) =>
+      res.push({
+        key: name,
+        value: name,
+        text: name,
+      })
+    );
     return res;
   };
 }
