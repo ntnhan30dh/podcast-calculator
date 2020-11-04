95a54875e590dd17e849d00cc0afd54b64214e0e
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2020-11-04 17:40:11 +0100

    styled v1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { Fragment, useState } from "react";
-import Calculator from './components/calculator';
-import Login from './components/login';
-import Logout from './components/logout';
-import Auth from './constants/auth';
-import 'semantic-ui-css/semantic.min.css'
-import './App.scss';
+import Calculator from "./components/calculator";
+import Login from "./components/login";
+import Logout from "./components/logout";
+import Auth from "./constants/auth";
 
+import logo from './images/logo.png'
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.scss";
 
 const auth = new Auth();
-const App =() =>{
+const App = () => {
   let [isAuthenticated, setstate] = useState(auth.isAuthenticated());
 
   const setLoginState = () => {
     setstate(auth.isAuthenticated());
   };
- 
+
   return (
     <Fragment className="App">
-    {!isAuthenticated && <Login setLoginState={setLoginState} />}
-    {isAuthenticated && (
-      <div>
-      <header className="App-header">
-      </header>
-      <Logout setLoginState={setLoginState}/>
-      <h1>Podcast  calculator</h1>
-      <Calculator/>
-      </div>
+      {!isAuthenticated && <Login setLoginState={setLoginState} />}
+      {isAuthenticated && (
+        <div className="wrap">
+          <header className="header">
+            <div className="left">
+              <div className="leftWrapper">
+              <div className="top">
+                  <img src={logo} alt="logo"/>
+                Podcast Cost Calculator
+              </div>
+              <div className="bottom">
+              <Logout setLoginState={setLoginState} />
+              </div>
+              </div>
+            </div>
+          </header>
+          <Calculator />
+        </div>
       )}
     </Fragment>
   );
-}
+};
 
 export default App;
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -50,78 +50,83 @@ const Calculator = () => {
   });
   return (
     <Form>
-      <h1>Calculator</h1>
-      <h3> Region</h3>
-      <Dropdown
-        placeholder="Select Region"
-        // fluid
-        search
-        selection
-        options={countryList.regions()}
-        // onChange={action}
-        onChange={(e, { value }) => setRegion(value)}
-      />
-      {isChecking && !region && (
-        <Label style={{ color: "red" }} basic color="red" pointing="left">
-          please choose region
-        </Label>
-      )}
-      {region && <h3>Country</h3>}
-      {/* <h4>{country}</h4> */}
-      {region && (
-        <Dropdown
-          placeholder="Select Country"
-          // fluid
-          search
-          selection
-          options={countryList.countries(region)}
-          onChange={(e, { value }) => setCountry(value)}
-        />
-      )}
-      {region && isChecking && !country && (
-        <Label style={{ color: "red" }} basic color="red" pointing="left">
-          please choose country
-        </Label>
-      )}
-      <h3>Payment model</h3>
-      {/* <h4>{paymentModel}</h4> */}
-      {/* <h3>Payment number</h3> */}
-      {/* <h4>
+      <div className="contentWrapper">
+        <div className="inputFlield">
+          <h3> Region</h3>
+          <Dropdown
+            placeholder="Select Region"
+            fluid
+            search
+            selection
+            options={countryList.regions()}
+            // onChange={action}
+            onChange={(e, { value }) => setRegion(value)}
+          />
+          {isChecking && !region && (
+            <Label style={{ color: "red" }} basic color="red" pointing="left">
+              please choose region
+            </Label>
+          )}
+          {region && <h3>Country</h3>}
+          {/* <h4>{country}</h4> */}
+          {region && (
+            <Dropdown
+              placeholder="Select Country"
+              fluid
+              search
+              selection
+              options={countryList.countries(region)}
+              onChange={(e, { value }) => setCountry(value)}
+            />
+          )}
+          {region && isChecking && !country && (
+            <Label style={{ color: "red" }} basic color="red" pointing="left">
+              please choose country
+            </Label>
+          )}
+          <h3>Payment model</h3>
+          {/* <h4>{paymentModel}</h4> */}
+          {/* <h3>Payment number</h3> */}
+          {/* <h4>
         {country && paymentModel && payment.getPayment(paymentModel)[country]}
       </h4> */}
-      {/* <Dropdown
+          {/* <Dropdown
         placeholder="Payment model"
         // fluid
         selection
         options={payments}
         onChange={(e, { value }) => setPaymentModel(value)}
       /> */}
-      <Form.Field>  
-      <Radio
-        label="CPM"
-        name="radioGroup"
-        value="cpm"
-        checked={paymentModel === "cpm"}
-        onChange={(e, { value }) => setPaymentModel(value)}
-      />
-      </Form.Field>  
-      <Form.Field> 
-      <Radio
-        label="CPA"
-        name="radioGroup"
-        value="cpa"
-        checked={paymentModel === "cpa"}
-        onChange={(e, { value }) => setPaymentModel(value)}
-      />
-      </Form.Field>  
-      {isChecking && !paymentModel && (
-        <Label style={{ color: "red" }} basic color="red" pointing="left">
-          please choose Payment model
-        </Label>
-      )}
-      <h3>Ad format</h3>
-      {/* <h4>{adFormat}</h4> */}
-      {/* <Dropdown
+          <div className="radios">
+            <Form.Field>
+              <Radio
+                className={paymentModel === "cpm" ? "active" : ""}
+                label="CPM"
+                name="radioGroup"
+                value="cpm"
+                checked={paymentModel === "cpm"}
+                onChange={(e, { value }) => setPaymentModel(value)}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                className={paymentModel === "cpa" ? "active" : ""}
+                label="CPA"
+                name="radioGroup"
+                value="cpa"
+                checked={paymentModel === "cpa"}
+                onChange={(e, { value }) => setPaymentModel(value)}
+              />
+            </Form.Field>
+          </div>
+          {isChecking && !paymentModel && (
+            <Label style={{ color: "red" }} basic color="red" pointing="left">
+              please choose Payment model
+            </Label>
+          )}
+          <h3>Ad format</h3>
+          {/* <h4>{adFormat}</h4> */}
+          {/* <Dropdown
         placeholder="Ad format"
         fluid
         selection
@@ -130,74 +135,105 @@ const Calculator = () => {
           value === "Host-read" ? setAdFormat(1) : setAdFormat(0.8)
         }
       /> */}
-      <Form.Field> 
-      <Radio
-        label="Host-read"
-        name="radioGroup1"
-        value="Host-read"
-        checked={adFormat === 1}
-        onChange={(e, { value }) => setAdFormat(1)}
-      />
-      </Form.Field>  
-      <Form.Field> 
-      <Radio
-        label="pre-recorded"
-        name="radioGroup1"
-        value="pre-recorded"
-        checked={adFormat === 0.8}
-        onChange={(e, { value }) => setAdFormat(0.8)}
-      />
-      </Form.Field>  
-      {isChecking && !adFormat && (
-        <Label style={{ color: "red" }} basic color="red" pointing="left">
-          please choose Ad format
-        </Label>
-      )}
-      {paymentModel === "cpm" && <h3>Reach</h3>}
-      {/* <h4>{reach}</h4> */}
-      {paymentModel === "cpm" && (
-        <Input onChange={(e, { value }) => setReach(value)} />
-      )}
-      {!/^[0-9.]*$/.test(reach) && reach && (
-        <Label basic color="red" pointing="left">
-          Please enter a number
-        </Label>
-      )}
-      {isChecking && !reach && paymentModel === "cpm" && (
-        <Label style={{ color: "red" }} basic color="red" pointing="left">
-          please enter Reach
-        </Label>
-      )}
-      {paymentModel === "cpm" && (
-        <h3>
-          Average monthly listners:{" "}
-          {country &&
-            aReach.aReachArr()[country] &&
-            aReach.aReachArr()[country].monthly}
-        </h3>
-      )}
-      {paymentModel === "cpm" && (
-        <h3>
-          Average weekly listners:{" "}
-          {country &&
-            aReach.aReachArr()[country] &&
-            aReach.aReachArr()[country].weekly}{" "}
-        </h3>
-      )}
+          <div className="radios">
+            <Form.Field>
+              <Radio
+                className={adFormat === 1 ? "active" : ""}
+                label="Host-read"
+                name="radioGroup1"
+                value="Host-read"
+                checked={adFormat === 1}
+                onChange={(e, { value }) => setAdFormat(1)}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                className={adFormat === 0.8 ? "active" : ""}
+                label="Pre-Recorded"
+                name="radioGroup1"
+                value="pre-recorded"
+                checked={adFormat === 0.8}
+                onChange={(e, { value }) => setAdFormat(0.8)}
+              />
+            </Form.Field>
+          </div>
+          {isChecking && !adFormat && (
+            <Label style={{ color: "red" }} basic color="red" pointing="left">
+              please choose Ad format
+            </Label>
+          )}
 
-      <h3>Number of spots</h3>
-      {/* <h4>{spot}</h4> */}
-      {/* <Input type='number' onChange={(e, { value }) => handelSetSpot(value)} /> */}
-      <Input onChange={(e, { value }) => setSpot(value)} />
-      {!/^\d+$/.test(spot) && spot && (
-        <Label basic color="red" pointing="left">
-          Please enter a valid number
-        </Label>
-      )}
-      <h2>
-        Estimated cost:{" "}
-        {country && paymentModel && spot && adFormat && formatter.format(cost)}
-      </h2>
+          {paymentModel === "cpm" && <h3>Reach</h3>}
+          {/* <h4>{reach}</h4> */}
+          {paymentModel === "cpm" && (
+            <Input
+              fluid
+              type="number"
+              onChange={(e, { value }) => setReach(value)}
+            />
+          )}
+          {!/^[0-9.]*$/.test(reach) && reach && (
+            <Label basic color="red" pointing="left">
+              Please enter a number
+            </Label>
+          )}
+          {isChecking && !reach && paymentModel === "cpm" && (
+            <Label style={{ color: "red" }} basic color="red" pointing="left">
+              please enter Reach
+            </Label>
+          )}
+
+          <h3>Number of spots</h3>
+          {/* <h4>{spot}</h4> */}
+          {/* <Input type='number' onChange={(e, { value }) => handelSetSpot(value)} /> */}
+          <Input
+            //inputmode="numeric" pattern="[0-9]*"
+            fluid
+            type="number"
+            onChange={(e, { value }) => setSpot(value)}
+          />
+          {!/^\d+$/.test(spot) && spot && (
+            <Label basic color="red" pointing="left">
+              Please enter a valid number
+            </Label>
+          )}
+        </div>
+        <div className="result">
+          {paymentModel === "cpm" && <h1> AVERAGE LISTENERS</h1>}
+          {paymentModel === "cpm" && (
+            <div className="aveListeners">
+              <div className="part">
+                <h3>Monthly</h3>
+                <h4>
+                  {" "}
+                  {country &&
+                    aReach.aReachArr()[country] &&
+                    aReach.aReachArr()[country].monthly}
+                </h4>
+              </div>
+              <div className="part">
+                <h3>Weekly</h3>
+                <h4>
+                  {country &&
+                    aReach.aReachArr()[country] &&
+                    aReach.aReachArr()[country].weekly}
+                </h4>
+              </div>
+            </div>
+          )}
+
+          <h1>
+            ESTIMATED COST
+            <h2>
+              {country &&
+                paymentModel &&
+                spot &&
+                adFormat &&
+                formatter.format(cost)}
+            </h2>
+          </h1>
+        </div>
+      </div>
     </Form>
   );
 };
